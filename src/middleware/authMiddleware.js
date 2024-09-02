5498d267d7e3c2d1d690a6d3e902a8c364c708a3
NhanLe <leanhnhan96@gmail.com>
2024-09-02 23:54:15 +0700

    feat: make auth for admin

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,10 +1,21 @@
-export const isAuthenticated = (req, res, next) => {
+export const isAuthenticatedUser = (req, res, next) => {
   // Check if user is logged in
-    if (req.session.user) {
-      // If user is logged in, call next middleware
-      next();
-    } else {
-      // If user is not logged in, redirect to login page
-      res.redirect('/user/login');
-    }
+  if (req.session.user) {
+    // If user is logged in, call next middleware
+    next();
+  } else {
+    // If user is not logged in, redirect to login page
+    res.redirect('/user/login');
+  }
+}
+
+export const isAuthenticatedAdmin = (req, res, next) => {
+  // Check if admin is logged in
+  if (req.session.admin) {
+    // If admin is logged in, call next middleware
+    next();
+  } else {
+    // If admin is not logged in, redirect to login page
+    res.redirect('/admin/login');
+  }
 }
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,20 +1,27 @@
 import express from 'express';
 // Import admin controller
 import AdminController from '../controllers/AdminController.js';
+import { isAuthenticatedAdmin } from '../middleware/authMiddleware.js';
 
 const adminRouter = express.Router();
 
 // Define your admin routes here
 
 // Define your routes here
+// Get login page
+adminRouter.get('/login', AdminController.getLoginPage);
+// Post login data
+adminRouter.post('/login', AdminController.doLogin);
+// Do logout
+adminRouter.post('/logout', AdminController.doLogout);
 // Get dashboard page
-adminRouter.get('/dashboard', AdminController.getDashboardPage);
+adminRouter.get('/dashboard',isAuthenticatedAdmin, AdminController.getDashboardPage);
 // Get add new user page
-adminRouter.get('/add-new-user', AdminController.getAddNewUserPage);
+adminRouter.get('/add-new-user',isAuthenticatedAdmin, AdminController.getAddNewUserPage);
 // Register new user
-adminRouter.post('/add-new-user', AdminController.registerNewUser);
+adminRouter.post('/add-new-user',isAuthenticatedAdmin, AdminController.registerNewUser);
 // Delete user
-adminRouter.post('/delete-user', AdminController.deleteUser);
+adminRouter.post('/delete-user',isAuthenticatedAdmin, AdminController.deleteUser);
 
 
 export default adminRouter;
