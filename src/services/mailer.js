583e20b13c86fa9acf3174816485a5c9dc28a9b1
NhanLe <leanhnhan96@gmail.com>
2024-09-03 23:23:31 +0700

    feat: add send mail function

---
+++ src/services/mailer.js
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  // Setting service send mail
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_FROM,
+    pass: process.env.MAIL_PASSWORD
+  }
+});
+
+async function sendVerificationEmail(userEmail, verificationToken) {
+  const verificationUrl = `${process.env.SITE_URL}/verify-email?token=${verificationToken}`;
+  const mailOptions = {
+    from: process.env.MAIL_FROM,
+    to: userEmail,
+    subject: 'Email Verification',
+    html: `
+    <p>Please verify your email by clicking the button below:</p>
+    <p>Password expires in 1 day</p>
+    <a href="${verificationUrl}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: white; background-color: #007BFF; text-decoration: none; border-radius: 5px;">Verify Email</a>
+    <p>If the button above does not work, please copy and paste the following URL into your browser:</p>
+    <p>${verificationUrl}</p>
+  `
+};
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Verification email sent to:', userEmail);
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+  }
+}
+
+export default sendVerificationEmail;
